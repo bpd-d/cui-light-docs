45d5760c29ddd9f8e0ffe39cb223881e91a92607
bartoszp32 <bartoszp32@gmail.com>
2020-07-16 20:54:12 +0200

    Overview and icons

---
+++ dist/src/components/docs.d.ts
@@ -0,0 +1,8 @@
+import * as React from "react";
+export interface DocsProps {
+    site?: string;
+}
+export declare class Docs extends React.Component<any, {}> {
+    constructor(props: any);
+    render(): JSX.Element;
+}
---
+++ dist/src/components/test.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+export interface TestComponentProps {
+    name: string;
+}
+export declare function TestComponent(props: TestComponentProps): JSX.Element;
