4968b1d71f2c056476d3b7878bd8a55ebf25e111
bartoszp32 <bartoszp32@gmail.com>
2021-02-08 10:36:39 +0100

    Update

--- src/components/navigation/offcanvas.tsx
+++ src/components/navigation/offcanvas.tsx
@@ -39,8 +39,8 @@ export function OffCanvas() {
 
 
     return <div className="cui-off-canvas" id="app-offcanvas" cui-off-canvas="escClose: y; outClose: y">
-        <div className="cui-off-canvas-container cui-card off-canvas-layout">
-            <div className="cui-card-header cui-flex cui-middle cui-between">
+        <div className="cui-off-canvas-container cui-card off-canvas-layout cui-flex cui-flex-direction-column cui-nowrap">
+            <div className="cui-card-header cui-flex cui-middle cui-between cui-flex-shrink cui-width-1-1">
                 <form action="" onSubmit={onSubmit}>
                     <div className="cui-input-mix">
                         <input type="text" className="cui-input" placeholder="Search" onChange={onSearchInputUpdate} onSubmit={onSubmit} />
@@ -49,8 +49,8 @@ export function OffCanvas() {
                 </form>
                 <a href="#" className="cui-icon" cui-icon="close" cui-close=""></a>
             </div>
-            <div className="off-canvas-content cui-card-body">
-                <div className="cui-width-1-1">
+            <div className="cui-card-body cui-flex-grow cui-overflow-hidden cui-width-1-1">
+                <div className="cui-width-1-1 cui-height-1-1 cui-flex cui-flex-direction-column cui-nowrap">
                     <h3 className="cui-h3">Menu</h3>
                     <ul className="cui-list cui-interactive cui-margin-top">
                         <li><NavbarLink url={ROUTES['home'].uri} name={ROUTES['home'].name} shouldClose={true} /></li>
@@ -59,11 +59,14 @@ export function OffCanvas() {
                         <li><NavbarLink url={ROUTES['icons'].uri} name={ROUTES['icons'].name} shouldClose={true} /></li>
                         <li><NavbarLink url={ROUTES['about'].uri} name={ROUTES['about'].name} shouldClose={true} /></li>
                     </ul>
-                    <h3 className="cui-h3">Components</h3>
-                    <CuiDocsNavigation shouldClose={true} sort={true} />
+                    <div className="cui-flex-grow cui-overflow-y-auto cui-width-1-1">
+                        <h3 className="cui-h3">Components</h3>
+                        <CuiDocsNavigation shouldClose={true} sort={true} />
+                    </div>
+
                 </div>
             </div>
-            <div className="cui-flex-center cui-card-footer">
+            <div className="cui-flex-center cui-card-footer cui-width-1-1">
                 <Link to={ROUTES['download'].uri} className="cui-button cui-accent cui-rounded">{ROUTES['download'].name}</Link>
             </div>
         </div>
